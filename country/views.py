c3bc3bc47c99bd37e1fcad0c46518072a8b5e262
Wan Zulmuhammad Harith <wzharith@gmail.com>
2021-07-19 09:52:49 +0800

    add template

--- country/views.py
+++ country/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    response = requests.get('https://restcountries.eu/rest/v2/all')
+    # transfor the response to json objects
+    todos = response.json()
+    return render(request, 'index.html', {"todos": todos})
